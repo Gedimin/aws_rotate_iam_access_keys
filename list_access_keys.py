6b6e42957dbc0ef663f5f859d33625ef1c5b3061
Aliaksei Anikiyenka <aliaksei.anikiyenka@epam.com>
2022-07-12 11:48:23 +0300

    Add age of access key

--- list_access_keys.py
+++ list_access_keys.py
@@ -3,15 +3,17 @@
 
 from list_profiles import get_list_profiles_output
 from aws_api import aws_connection_context
+from datetime import datetime, timezone
 
 
 def get_list_access_keys(conn_context):
+    now = datetime.now(timezone.utc)
     list_access_keys = conn_context.list_access_keys().get('AccessKeyMetadata')
     for key in list_access_keys:
         username = key.get('UserName')
         access_key_id = key.get('AccessKeyId')
         created_date = key.get('CreateDate')
-        print(f'[INFO] - AccessKey: Username: {username}, AccessKeyId: {access_key_id} Created: {created_date}')
+        print(f'[INFO] - AccessKey: Username: {username}, AccessKeyId: {access_key_id} Created: {created_date}, Age: {now - created_date}')
     return list_access_keys
 
 
